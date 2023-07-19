14b298bc441713a79d42b53c5035e6f51cf7e53e
Prathyush s panicker <panickerprathyush20@gmail.com>
2023-07-19 15:02:29 +0530

    final

--- minElement.java
+++ minElement.java
@@ -7,11 +7,22 @@ public class minElement {
             if(arr[i]<min)
             min=arr[i];
         }
-        System.out.println("Minumum element is "+min);
+        System.out.println("Minimum element is "+min);
+    }
+    public static void max(int arr[])
+    {
+        int max=arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            max=arr[i];
+        }
+        System.out.println("Maximum element is "+max);
     }
     public static void main(String[] args)
     {
         int a[]={5,1,6,7,2};
         min(a);
+        max(a);
     }
 }
