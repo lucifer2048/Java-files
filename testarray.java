476c8172556ee08bb41235ffb8c8e6fb5c2d906a
Prathyush s panicker <panickerprathyush20@gmail.com>
2023-07-19 14:23:53 +0530

    updated

--- testarray.java
+++ testarray.java
@@ -8,7 +8,13 @@ public class testarray
         a[2]=30;
         a[3]=40;
         a[4]=50;
+        System.out.println("a: ");
         for(int i=0;i<a.length;i++)
-        System.out.println(a[i]);
+        System.out.print(a[i]+" ");
+        System.out.println();
+        int b[]={2,4,6,8,10};
+        System.out.println("b: ");
+        for(int i=0;i<b.length;i++)
+        System.out.print(b[i]+" ");
     }
 } 
